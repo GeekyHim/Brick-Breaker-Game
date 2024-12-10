96841decea7d7f4b3057036bba7163cc3ba0f1dc
GeekyHim <work.himanshu1720@gmail.com>
2024-12-10 23:45:24 +0530

    wroking

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,6 +1,86 @@
 import javax.swing.JPanel;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+//panel where we run our game
+public class Gameplay extends JPanel implements KeyListener,ActionListener  {
+    // basic properties
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21; //defines the no of bricks and hence the map
 
-public class Gameplay extends JPanel{
-    //panel where we run our game
+    private Timer timer; // ball ko time and speed dene ke liye
+    private int delay = 10;
+
+    //player and ball positions
+    private int playerX = 310;
+    
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    // constructor 
+    public Gameplay(){
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        //timer = new Timer(delay,this);
+        //timer.start();
+
+
+    }
+
+    public void paint(Graphics g){
+        //ye graphic object receive karega jiski help se hum bricks, slider etc sab banayege
+        //background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        //border
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+        
+        //paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        //the ball
+        g.setColor(Color.yellow);
+        g.fillRect(ballPosX, ballPosY, 20, 20);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+    
+        
     
 }
